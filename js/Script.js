f898405738c9aadf18dc613799f990c037c03630
Aigerim <Lipricone@gmail.com>
2021-02-05 18:31:34 +0600

    Praktika

--- js/Script.js
+++ js/Script.js
@@ -1,4 +1,19 @@
 "use strict";
-//alert('Hello');
-const result=confirm ('Are you here?');
-console.log(result);
+alert('Hello');
+let numberOffilms=prompt ('Сколько фильмов вы посмотрели?','');
+console.log(numberOffilms);
+let personalMovieDB={
+     count:numberOffilms,
+     movies:{},
+     actors:{},
+     genres:{},
+     private:false
+};
+
+let pervyi=prompt("Один из последних просмотренных фильмов?", ''),
+    vtoroi=prompt("На сколько его оцените?", ''),
+    q=prompt("Один из последних просмотренных фильмов?", ''),
+    w=prompt("На сколько его оцените?", '')
+personalMovieDB.movies[q]=w;  
+personalMovieDB.movies[pervyi]=vtoroi;  
+console.log(personalMovieDB);
