5b6eb12bbe6d905f81d153f763507ba763b79374
Aigerim <lipricone@gmail.com>
2021-03-26 16:37:05 +0600

    ES6.1

--- js/Script.js
+++ js/Script.js
@@ -8,11 +8,18 @@ const options={
         bg: 'red'
     }
 };
-console.log(options.width);
-delete options.name;
+// console.log(options.width);
+// delete options.name;
 
-console.log(options);
+// console.log(options);
 
 for (let key in options){
-console.log (`Свойство ${key} имеет значение ${options[key]}`);
+    if (typeof(options[key]==='object')){ 
+        for(let i in options[key]){
+            console.log (`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+
+            console.log (`Свойство ${key} имеет значение ${options[key]}`);
+    }
 }
