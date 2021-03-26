798a1e3e138b942b9420ee7dc78f1f77a44c13b5
Aigerim <lipricone@gmail.com>
2021-03-26 17:07:16 +0600

    Object.keys

--- js/Script.js
+++ js/Script.js
@@ -8,16 +8,4 @@ const options={
         bg: 'red'
     }
 };
-// console.log(options.width);
-// delete options.name;
-// console.log(options);
-for (let key in options){
-    if (typeof(options[key]) === 'object'){ 
-        for(let i in options[key]){
-            console.log (`Свойство ${i} имеет значение ${options[key][i]}`);
-        }
-    } else {
-
-            console.log (`Свойство ${key} имеет значение ${options[key]}`);
-    }
-}
+console.log(Object.keys(options));
