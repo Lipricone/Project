b4fba86e48863403c2b5a204dfedc45c23334a23
Aigerim <lipricone@gmail.com>
2021-03-30 16:43:12 +0600

    SplitJoinSort

--- js/Script.js
+++ js/Script.js
@@ -6,6 +6,13 @@ const arr=[1, 2, 3, 6, 8];
 // for (let i=0;i<arr.length;i++){
 //     console.log(arr[i]);
 // }
-for (let value of arr){
-    console.log(value)
-}
+// for (let value of arr){
+//     console.log(value)
+// }
+// arr.forEach(function(item, i, arr){
+//     console.log(`${i}: ${item} внутри массива ${arr} `);
+// });
+  const str=prompt("","");
+  const products= str.split(", ");
+  products.sort();
+  console.log(products.join('; '));
