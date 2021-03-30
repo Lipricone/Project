85604d712963c04dc3d859616c7d223afebcfdc6
Aigerim <lipricone@gmail.com>
2021-03-30 15:31:23 +0600

    ForOfArray

--- js/Script.js
+++ js/Script.js
@@ -1,5 +1,11 @@
 "use strict";
 const arr=[1, 2, 3, 6, 8];
-arr.pop();
-arr.push(10);
-console.log(arr);
+// arr.pop();
+// arr.push(10);
+// console.log(arr);
+// for (let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+for (let value of arr){
+    console.log(value)
+}
