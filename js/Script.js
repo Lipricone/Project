e461068355a15c58a3c4965778fae2653e54564b
Aigerim <lipricone@gmail.com>
2021-03-31 18:57:53 +0600

    Object create

--- js/Script.js
+++ js/Script.js
@@ -6,10 +6,4 @@ const soldier = {
         console.log("Hello")
     }
 };
-const john = { 
-    health: 100
-};
-
-john.__proto__ = soldier;
-john.sayHello();
-console.log(john.sayHello);
+const john=Object.create(soldier)  
